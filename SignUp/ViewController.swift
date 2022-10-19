8d1b49246c0b027c1a103097afe49dbe013aaa95
AnnKangHo <duddnd6897@gmail.com>
2022-10-19 15:23:06 +0900

    입력 안할 시 이벤트 1

--- SignUp/ViewController.swift
+++ SignUp/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var idTextFieldDescription: UILabel!
+    
+    @IBOutlet var defaultHiddenCollection: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for label in defaultHiddenCollection {
+            label.isHidden = true
+        }
+    }
+    @IBAction func IdTextFieldTyped(_ sender: UITextField) {
+        idTextFieldDescription.text = "아이디는 필수입력 정보입니다."
+        idTextFieldDescription.isHidden =  false
     }
 
 
