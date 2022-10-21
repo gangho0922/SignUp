37566b4933fc0c179c6ca1537ebf0fd12da936b6
AnnKangHo <duddnd6897@gmail.com>
2022-10-21 23:47:50 +0900

    회원가입 아이디 이벤트 1 (시험본)

--- SignUp/ViewController.swift
+++ SignUp/ViewController.swift
@@ -20,11 +20,34 @@ class ViewController: UIViewController {
             label.isHidden = true
         }
     }
-    @IBAction func IdTextFieldTyped(_ sender: UITextField) {
-        idTextFieldDescription.text = "아이디는 필수입력 정보입니다."
-        idTextFieldDescription.isHidden =  false
-    }
+    @IBAction func idTextFieldTyped(_ sender: UITextField) {
+           idTextFieldDescription.isHidden = false // label 보여주기
+           
+           let userWord = idTextField.text?.lowercased() // 소문자 변환
+           idTextField.text = userWord // 변환된 소문자로 바꿔주기
+           
+           let minCount = 5
+           let maxCount = 12
+           let count = userWord!.count
 
+           switch count {
+           case 0:
+               idTextFieldDescription.text = "아이디는 필수입력 정보입니다."
+           case 1..<minCount:
+               idTextFieldDescription.text = "아이디는 5글자 이상이어야 합니다."
+           case minCount...maxCount:
+               let idPattern = "^[a-z0-9-_]{\(minCount),\(maxCount)}$"
+               let isVaildPattern = (userWord!.range(of: idPattern, options: .regularExpression) != nil)
+               if isVaildPattern {
+                   idTextFieldDescription.text = "조건에 맞는 아이디"
+                   idTextFieldDescription.isHidden = true
+               } else {
+                   idTextFieldDescription.text = "소문자, 숫자, 빼기(-), 밑줄(_)만 사용할 수 있습니다."
+               }
+           default:
+               idTextFieldDescription.text = "아이디는 12글자 이하이어야 합니다."
+           }
+    }
 
 }
 
